65af5c6af64ac5b9909d7928e5844e2880424b8e
darrellp <darrellp@b45b8156-6e7d-4523-9bb7-f99d666815c7>
2014-07-05 16:42:20 +0000

    Testing passing in a compare function

--- Priority Queue Tests/FibonacciQueueTests.cs
+++ Priority Queue Tests/FibonacciQueueTests.cs
@@ -118,6 +118,68 @@ namespace Priority_Queue_Tests
 		}
 
 		[TestMethod]
+		public void TestRandomOps2()
+		{
+			var rnd = new Random(110456);
+			var fpq = new FibonacciPriorityQueue<Pqt<int>>((i1, i2) => i1.CompareTo(i2));
+			var vals = new HashSet<Pqt<int>>();
+			Pqt<int> val;
+
+			for (var i = 0; i < 1000; i++)
+			{
+				Assert.AreEqual(fpq.Count, vals.Count);
+				if (vals.Count == 0 || rnd.Next(100) > 30)
+				{
+					val = rnd.Next();
+					if (vals.Contains(val))
+					{
+						// Since the vals set would only keep one copy of the value this would
+						// screw us up if it were allowed to happen
+						continue;
+					}
+					vals.Add(fpq.AddTyped(val));
+				}
+				else if (rnd.Next(100) < 5)
+				{
+					val = vals.First();
+					fpq.DeleteTyped(val);
+					vals.Remove(val);
+				}
+				else if (rnd.Next(100) < 20)
+				{
+					val = vals.First();
+					Pqt<int> newval = rnd.Next(val);
+					// DecreaseKeyTyped will transfer cookie value from the old to the
+					// new FpqInt.
+					fpq.DecreaseKeyTyped(val, newval);
+					vals.Remove(val);
+					vals.Add(newval);
+					Assert.IsTrue(fpq.Validate());
+				}
+				else
+				{
+					val = fpq.Pop();
+					Assert.IsTrue(vals.Contains(val));
+					vals.Remove(val);
+				}
+			}
+			Assert.AreEqual(vals.Count, fpq.Count);
+			Assert.AreEqual(vals.Count, fpq.ToList().Count);
+			val = fpq.Pop();
+			Assert.IsTrue(vals.Contains(val));
+			vals.Remove(val);
+			var count = vals.Count;
+			for (var i = 0; i < count; i++)
+			{
+				var next = fpq.Pop();
+				Assert.IsTrue(next > val);
+				Assert.IsTrue(vals.Contains(next));
+				vals.Remove(next);
+				val = next;
+			}
+		}
+
+		[TestMethod]
 		public void TestDecreaseKey()
 		{
 			var fpq = new FibonacciPriorityQueue<Pqt<int>>();
